0362d8a07d481a0357e4b66c362acebcaff14e0d
yuksel.ozdemir <yuksel.ozdemir@dolap.com>
2021-11-28 11:15:31 +0300

    edit config.go

--- config.go
+++ config.go
@@ -1,48 +1,85 @@
 package parseandgo
 
+import "errors"
+
+var ErrKeyNotFound = errors.New("given key not exists in configuration")
+
 type Config map[string]interface{}
 
 func (c Config) GetValueAsBool(key string) bool {
+	foundKey, err := getType(key, c)
+	if err != nil {
+		logError(err)
+	}
 	defer func() {
-		if err := recover(); err != nil {
-			logPanic(err)
+		if e := recover(); e != nil {
+			logPanic(e)
 		}
 	}()
-	return c[key].(bool)
+	return foundKey.(bool)
 }
 
 func (c Config) GetValueAsInt(key string) int {
+	foundKey, err := getType(key, c)
+	if err != nil {
+		logError(err)
+	}
 	defer func() {
-		if err := recover(); err != nil {
-			logPanic(err)
+		if e := recover(); e != nil {
+			logPanic(e)
 		}
 	}()
-	return c[key].(int)
+	return foundKey.(int)
 }
 
 func (c Config) GetValueAsString(key string) string {
+	foundKey, err := getType(key, c)
+	if err != nil {
+		logError(err)
+	}
 	defer func() {
-		if err := recover(); err != nil {
-			logPanic(err)
+		if e := recover(); e != nil {
+			logPanic(e)
 		}
 	}()
-	return c[key].(string)
+	return foundKey.(string)
 }
 
 func (c Config) GetValueAsFloat(key string) float64 {
+	foundKey, err := getType(key, c)
+	if err != nil {
+		logError(err)
+	}
 	defer func() {
-		if err := recover(); err != nil {
-			logPanic(err)
+		if e := recover(); e != nil {
+			logPanic(e)
 		}
 	}()
-	return c[key].(float64)
+	return foundKey.(float64)
 }
 
 func (c Config) GetValueAsConfig(key string) Config {
+	foundKey, err := getType(key, c)
+	if err != nil {
+		logError(err)
+	}
 	defer func() {
-		if err := recover(); err != nil {
-			logPanic(err)
+		if e := recover(); e != nil {
+			logPanic(e)
 		}
 	}()
-	return c[key].(Config)
+	return foundKey.(Config)
+}
+
+func getType(_key string, config Config) (interface{}, error) {
+	for key, value := range config {
+		if key == _key {
+			return value, nil
+		}
+		switch typedValue := value.(type) {
+		case Config:
+			return getType(_key, typedValue)
+		}
+	}
+	return nil, ErrKeyNotFound
 }
